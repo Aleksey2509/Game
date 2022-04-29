eb3da3720659bd99d90e63c0bd6982a448d93534
MrYar0s <molokan.yaroslavik@gmail.com>
2022-04-29 15:02:59 +0300

    Add StateMachine class

---
+++ Game/StateMachine/StateMachine.cpp
@@ -0,0 +1,70 @@
+#include "StateMachine.hpp"
+
+StateMachine::StateMachine(sf::RenderWindow& window, float dt, float fixdt) :
+	window_(window), dt_(dt), fixdt_(fixdt)
+{
+	auto params = window_.getSize();
+	frame_.create(params.x, params.y);
+}
+
+StateMachine::~StateMachine()
+{
+	while(~states_.empty())
+	{
+		delete states_.top();
+		states_.pop();
+	}
+}
+
+void StateMachine::PushStat(State* state)
+{
+	states_.push(state);
+}
+
+void StateMachine::FixedUpdate()
+{
+	if(!states_.empty())
+	{
+		states_.top()->FixedUpdate();
+	}
+}
+
+void StateMachine::Update()
+{
+	if(!states_.empty())
+	{
+		states_.top()->Update();
+		if(!states_.top()->IsRunning())
+		{
+			deleete states_.top();
+			states_.pop();
+		}
+	}
+	else
+	{
+		window_.close();
+	}
+}
+
+void StateMachine::Render()
+{
+	frame_.clear();
+
+	if(!states_.empty())
+	{
+		states_.top()->Render(frame_);
+	}
+	frame_.display();
+	sprite_.setTexture(frame.getTexture());
+	window_.draw(sprite_);
+}
+
+State::State(StateMachine& machine) :
+machine_(machine), window_(machine.window_), dt_(machine.dt_), fixdt_(machine.fixdt_) {}
+
+State::~State() {}
+
+bool IsRunning() const
+{
+	return isRun;
+}
